083b44c2d783ae71072f0424583c77a40315cabe
Anurak2534 <126850163+Anurak2534@users.noreply.github.com>
2023-03-09 20:11:27 +0700

    Update ohmvpn.php

--- ohmvpn.php
+++ ohmvpn.php
@@ -1,8 +1,16 @@
-1.1
+1.2
 OHM VPN
-อัพเดตไฟล์ 04-03-23
-เซิฟVIP-CSL 6 SERVER
-BY OHM VPN
+📥FILE UPDATE 09-03-23
+🌐SERVER LIST
+🔥HOMENET 2 SERVER
+🌟VIP-CAT 2 SERVER
+🌟VIP-GAMING 2 SERVER
+BY ITIM VPN
+📢เซิร์ฟเวอร์จะrebootเวลา03:30น.ทุกวัน
+✔กันรั่วซิมทรูกด*935*99# ฟรี
+✔กันรั่วซิมดีแทคกด*117*30# 20บาท
+✔กันรั่วซิมAISกด*777*7067# 31บาท
+
 
 
 
