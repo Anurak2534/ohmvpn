9bf4b26eef9ae4a86267168f583a39c623dec1aa
Anurak2534 <126850163+Anurak2534@users.noreply.github.com>
2023-04-06 21:48:30 +0700

    Update ohmvpn.php

--- ohmvpn.php
+++ ohmvpn.php
@@ -1,13 +1,8 @@
-1.5
+1.6
 OHM VPN
-📥FILE UPDATE 21-03-23
+📥FILE UPDATE 06-04-23
 🌐SERVER LIST
-🔥HOMENET 2 SERVER
 🌟VIP-CAT 2 SERVER
-🌟VIP-GAMING 2 SERVER
-🌟VIP-INET 2 SERVER
-👉 ชื่อผู้ใช้:30day
-   รหัสผ่าน:12345
 BY ITIM VPN
 📢เซิร์ฟเวอร์จะrebootเวลา03:30น.ทุกวัน
 ✔กันรั่วซิมทรูกด*935*99# ฟรี
