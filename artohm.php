5f6de21179bd15226a2d16557101a0da99e6c25c
Anurak2534 <126850163+Anurak2534@users.noreply.github.com>
2023-08-12 18:01:08 +0700

    Update artohm.php

--- artohm.php
+++ artohm.php
@@ -1,18 +1,15 @@
-4.0
+4.1
 ✨OHM VPN✨
-📥FILE UPDATE 10-08-23
-🌟เพิ่มเซิร์ฟ COMBO 1 SERVER
-🌟เพิ่มเซิร์ฟ TH-CSL 3 SERVER
-🌟เพิ่มเซิร์ฟ TH-MC 3 SERVER
+📥FILE UPDATE 12-08-23
+🌟เพิ่มเซิร์ฟ SG 2 SERVER
 🌐SERVER LIST
 🌟COMBO 16 SERVER ใช้งานทั่วไป
-🌟SG 3 SERVER ใช้งานทั่วไป 
+🌟SG 5 SERVER ใช้งานทั่วไป 
 🌟TH-BM 3 SERVER ใช้เล่นเกมส์เท่านั้น
 🌟TH-CSL 3 SERVER ใช้เล่นเกมส์ ท่องเว็บ 
 🌟TH-INET 5 SERVER ใช้เล่นเกมส์เท่านั้น
-🌟VIP-CAT 40 SERVER ใช้งานทั่วไป
+🌟VIP-CAT 30 SERVER ใช้งานทั่วไป
 🌟VIP-GAMING 20 SERVER ใช้เล่นเกมส์เท่านั้น 
-🌟VIP-YOUTUBE 10 SERVER ใช้ดูยูทูบลื่นๆ
 ♥BY ITIM VPN
 📢เซิร์ฟเวอร์จะrebootเวลา03:30น.ทุกวัน
 ✔กันรั่วซิมทรูกด*935*99# ฟรี
