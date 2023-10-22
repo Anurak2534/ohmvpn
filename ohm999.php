27af9babda443f19d42dd7f41efea1980211e6f0
Anurak2534 <126850163+Anurak2534@users.noreply.github.com>
2023-10-22 20:13:21 +0700

    Create ohm999.php

---
+++ ohm999.php
@@ -0,0 +1,14 @@
+5.6
+✨OHM VPN✨
+📥FILE UPDATE 23-10-23
+🌐SERVER LIST
+🌟TH-MT 5 SERER ใช้ท่องเว็บ เล่นเกมส์
+🌟TH-CAT 10 SERVER ใช้งานทั่วไป
+🌟COMBO 10 SERVER ใช้งานทั่วไป
+🌟SG 10 SERVER ใช้งานทั่วไป 
+🌟TH-OPENLAND 10 SERVER ใช้เล่นเกมส์เท่านั้น
+🌟VIP-CAT 25 SERVER ใช้งานทั่วไป
+🌟VIP-GAMING 25 SERVER ใช้เล่นเกมส์เท่านั้น 
+📢เซิร์ฟเวอร์จะrebootเวลา03:30น.ทุกวัน
+✔กันรั่วซิมทรูกด*935*99# ฟรี
+✔กันรั่วซิมดีแทคกด*117*30# 20บาท
