287043e9dbe67c764939d97a33673a15181ce149
Anurak2534 <126850163+Anurak2534@users.noreply.github.com>
2023-11-27 05:28:58 +0700

    Update ohm999.php

--- ohm999.php
+++ ohm999.php
@@ -1,12 +1,10 @@
-6.4
+6.5
 ✨OHM VPN✨
-📥FILE UPDATE 17-11-23
+📥FILE UPDATE 27-11-23
 📌ซิมทรูโปรESPORTS
 📌ซิมดีแทคโปรLINE
 🌐SERVER LIST
 🌟TH-CAT 10 SERVER ใช้งานทั่วไป
-🌟COMBO 10 SERVER ใช้งานทั่วไป
-🌟SG 10 SERVER ใช้งานทั่วไป
 🌟VIP-CAT 10 SERVER ใช้งานทั่วไป
 🌟VIP-GAMING 10 SERVER ใช้เล่นเกมส์เท่านั้น 
 📢เซิร์ฟเวอร์จะrebootเวลา03:30น.ทุกวัน
